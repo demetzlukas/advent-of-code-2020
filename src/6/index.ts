97bcbc398428417a818a01769bc470f80f29c956
Lukas Demetz <lukas.demetz@fh-kufstein.ac.at>
2020-12-06 10:48:18 +0100

    Refactoring

--- src/6/index.ts
+++ src/6/index.ts
@@ -4,19 +4,19 @@ import * as _ from 'lodash';
 const fileName = './input/6.txt';
 
 export async function main() {
-    const answersPerGroups = await (
+    const answersPerGroupAndPerson = await (
         await readLinesFromInput(fileName, '\r\n\r\n')
     ).map(group => group.split(/\s+/).map(g => g.split('')));
 
     console.log(
-        `Part 1: ${answersPerGroups
+        `Part 1: ${answersPerGroupAndPerson
             .map(group => _.flatten(group))
             .map(answers => new Set(answers).size)
             .reduce((sum, current) => sum + current)}`
     );
 
     console.log(
-        `Part 2: ${answersPerGroups
+        `Part 2: ${answersPerGroupAndPerson
             .map(a => getNumberOfSharedAnswers(a))
             .reduce((sum, current) => sum + current)}`
     );
