de31f336d4178ca5db76a802ba6e403fdb61349e
Lukas Demetz <lukas.demetz@fh-kufstein.ac.at>
2020-12-06 10:47:14 +0100

    Solution day 6

---
+++ src/6/index.ts
@@ -0,0 +1,32 @@
+import { readLinesFromInput } from '../utils/readFile';
+import * as _ from 'lodash';
+
+const fileName = './input/6.txt';
+
+export async function main() {
+    const answersPerGroups = await (
+        await readLinesFromInput(fileName, '\r\n\r\n')
+    ).map(group => group.split(/\s+/).map(g => g.split('')));
+
+    console.log(
+        `Part 1: ${answersPerGroups
+            .map(group => _.flatten(group))
+            .map(answers => new Set(answers).size)
+            .reduce((sum, current) => sum + current)}`
+    );
+
+    console.log(
+        `Part 2: ${answersPerGroups
+            .map(a => getNumberOfSharedAnswers(a))
+            .reduce((sum, current) => sum + current)}`
+    );
+}
+
+function getNumberOfSharedAnswers(answers: string[][]): number {
+    let sharedAnswers = [];
+
+    answers.forEach(a => (sharedAnswers = [...sharedAnswers, ..._.flatten(a)]));
+    answers.forEach(a => (sharedAnswers = _.intersection(sharedAnswers, a)));
+
+    return sharedAnswers.length;
+}
