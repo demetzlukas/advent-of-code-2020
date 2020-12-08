c313ac2c5a64afa284dc0748d302176209b2cc9c
Lukas Demetz <lukas.demetz@fh-kufstein.ac.at>
2020-12-08 22:22:02 +0100

    Moved changing of operations to outer function

--- src/8/index.ts
+++ src/8/index.ts
@@ -5,38 +5,30 @@ const fileName = './input/8.txt';
 export async function main() {
     const input = await readLinesFromInput(fileName, '\r\n');
 
-    console.log(`Part 1: ${getValueOfAccumulator(input, -1).accumulator}`);
+    console.log(`Part 1: ${getValueOfAccumulator(input).accumulator}`);
     console.log(
         `Part 2: ${getValueOfAccumulatorWithChangedInstructions(input)}`
     );
 }
 
-function getValueOfAccumulator(lines: string[], operationToChange: number) {
+function getValueOfAccumulator(lines: string[]) {
     let instructions = lines.map(line => {
         const [type, value] = line.split(' ');
         return { type, value: parseInt(value), executed: false };
     });
-    let operationToChangeCounter = 0;
+
     let accumulator = 0;
     let index = 0;
     let line: { type: string; value: number; executed: boolean };
+
     while (
         index < instructions.length &&
         !(line = instructions[index]).executed
     ) {
-        if (operationToChangeCounter === operationToChange) {
-            if (line.type === 'nop') {
-                line.type = 'jmp';
-            } else if (line.type === 'jmp') {
-                line.type = 'nop';
-            }
-        }
-
         line.executed = true;
         switch (line.type) {
             case 'nop':
                 index++;
-                operationToChangeCounter++;
                 break;
             case 'acc':
                 accumulator += line.value;
@@ -44,7 +36,6 @@ function getValueOfAccumulator(lines: string[], operationToChange: number) {
                 break;
             case 'jmp':
                 index += line.value;
-                operationToChangeCounter++;
                 break;
 
             default:
@@ -56,13 +47,20 @@ function getValueOfAccumulator(lines: string[], operationToChange: number) {
 }
 
 function getValueOfAccumulatorWithChangedInstructions(lines: string[]) {
-    let operationToChange = 0;
-    while (operationToChange < lines.length) {
-        const { accumulator, index } = getValueOfAccumulator(
-            lines,
-            operationToChange
-        );
-        if (index == lines.length) return accumulator;
-        operationToChange++;
+    for (let i = 0; i < lines.length; i++) {
+        let linesCopy = [...lines];
+
+        if (linesCopy[i].startsWith('jmp')) {
+            linesCopy[i] = linesCopy[i].replace('jmp', 'nop');
+        } else if (linesCopy[i].startsWith('nop')) {
+            linesCopy[i] = linesCopy[i].replace('nop', 'jmp');
+        } else {
+            continue;
+        }
+
+        const { accumulator, index } = getValueOfAccumulator(linesCopy);
+
+        if (index == linesCopy.length) return accumulator;
     }
+    return -1;
 }
